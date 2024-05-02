4ef67e65482d5fccfcef6520eb9e0632af597c17
LiigaSoolane <mirjam.prayer@tu-dortmund.de>
2024-05-02 15:57:00 +0200

    deleted units

--- variables.py
+++ variables.py
@@ -18,16 +18,16 @@ def get_global_variables():
     return global_vars
 
 # B-field specifications
-B_middle = UnitFloat(87.3, "milli Tesla") # mT
-mu_0 = UnitFloat(1.25663706212*10**(-6), "Newton per Ampere squared") #magnetic permeability, N/A²
+B_middle = float(87.3) # mT
+mu_0 = float(1.25663706212*10**(-6)) #magnetic permeability, N/A²
 
 # Dimension restrictions
-max_radius = UnitFloat(0.8, "meter") #meter
-max_height = UnitFloat(0.8, "meter") #meter
-max_aspect_ratio = UnitFloat(10.) #no dimension
-min_aspect_ratio = UnitFloat(6.) #no dimension
-major_radius_plasma = UnitFloat(0.5, "meter") #meter
-minor_radius_plasma = UnitFloat(80., "milli meter") #mm
+max_radius = float(0.8) #meter
+max_height = float(0.8) #meter
+max_aspect_ratio = float(10.) #no dimension
+min_aspect_ratio = float(6.) #no dimension
+major_radius_plasma = float(0.5) #meter
+minor_radius_plasma = float(80.) #mm
 
 # coil assumptions
 number_of_coils_per_circuit = int(10)
@@ -35,32 +35,32 @@ number_of_circuits = int(4)
 number_of_windings = int(12)
 number_of_windings_x = int(3)
 number_of_wingings_y = int(4)
-max_current_per_mm_2 = UnitFloat(50., "Ampere per square milli meter") #A/mm²
-specific_resistance = UnitFloat(0.) #
-major_winding_radius = UnitFloat(160., "milli meter") #mm
-winding_radius = UnitFloat(0., "milli meter")
-total_winding_radius = UnitFloat(0., "milli meter") #mm, coil + cooling 
+max_current_per_mm_2 = float(50.) #A/mm²
+specific_resistance = float(0.) #
+major_winding_radius = float(0.16) #m
+winding_radius = float(0.) 
+total_winding_radius = float(0.) #mm, coil + cooling 
 
 input_variables = get_global_variables()
 
 # calculated from basis variables
-max_current_per_winding = UnitFloat(0., "Ampere") #A
-length_of_winding = UnitFloat(0., "meter") #m
-length_of_coil = UnitFloat(0., "meter") #m
-length_of_circuit = UnitFloat(0., "meter") #m
-current_within = UnitFloat(0., "kilo Ampere") #kA, within labels the stuff which is at positions < major radius
-radius_within = UnitFloat(0., "milli meter") #mm
-total_coil_radius_within = UnitFloat(0., "milli meter") #mm
-total_coil_volume_within = UnitFloat(0., "milli meter squared") #mm²
+max_current_per_winding = float(0.) #A
+length_of_winding = float(0.)#m
+length_of_coil = float(0.) #m
+length_of_circuit = float(0.) #m
+current_within = float(0.) #kA, within labels the stuff which is at positions < major radius
+radius_within = float(0.) #mm
+total_coil_radius_within = float(0.) #mm
+total_coil_volume_within = float(0.) #mm²
 number_of_coils = int(0)
 
 #calculated from precalculations
-current_per_coil = UnitFloat(0., "kilo Ampere") #kA
-current_per_winding = UnitFloat(0., "kilo Ampere") #kA
-volume_within = UnitFloat(0., "milli meter squared") #mm²
-aspect_ratio = UnitFloat(0.) #no dimension
-resistance_per_circuit = UnitFloat(0., "Ohm") #ohm
-voltage_per_circuit = UnitFloat(0., "Volt") #Volt
+current_per_coil = float(0.) #kA
+current_per_winding = float(0.) #kA
+volume_within = float(0.) #mm²
+aspect_ratio = float(0.) #no dimension
+resistance_per_circuit = float(0.) #ohm
+voltage_per_circuit = float(0.) #Volt
 
 #make a list of all variables
 all_variables =  get_global_variables()
