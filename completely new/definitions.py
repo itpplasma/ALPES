cd33e2b733c22952a511b5ea464bdaf9fd713fb0
LiigaSoolane <mirjam.prayer@tu-dortmund.de>
2024-05-07 10:43:39 +0200

    variablenamen geändert

--- completely new/definitions.py
+++ completely new/definitions.py
@@ -1,28 +1,41 @@
 import math
 import sys
 
-class StelleratorDesign:
+class StellaratorDesign:
     '''a wonderfull class to descibe the whole system'''
-    def __init__(self, B_middle=None, mu_0=None, max_radius=None, max_height=None,
-                 max_aspect_ratio=None, min_aspect_ratio=None, major_radius_plasma=None,
-                 minor_radius_plasma=None, number_of_coils_per_circuit=None,
+    def __init__(self, material, diam_max=None, max_height=None,
+                 max_aspect_ratio=None, min_aspect_ratio=None, radius_major=None,
+                 radius_minor=None, frequency_rotation=None,
+                  number_of_coils_per_circuit=None,
                  number_of_circuits=None, number_of_windings_x=None, number_of_windings_y=None,
                  max_current_per_m_2=None, specific_resistance=None, major_winding_radius=None,
                  winding_radius=None, cooling_radius=None):
         
-        # B-field specifications
-        self.B_middle = B_middle if B_middle is not None else float(87.3/1000) # T
-        self.mu_0 = mu_0 if mu_0 is not None else float(1.25663706212*10**(-6)) # magnetic permeability, N/A²
+        #constants
+        self.mu_0 = float(1.25663706212e-6) # magnetic permeability, N/A²
+        self.electron_mass = float(9.1093837015e-31) #kg
+        self.electron_charge = float(1.602176634e-19) #C
         
         # Dimension restrictions
-        self.max_radius = max_radius if max_radius is not None else float(0.8) # meter
+        self.diam_max = diam_max if diam_max is not None else float(1.8) # meter
         self.max_height = max_height if max_height is not None else float(0.8) # meter
         self.max_aspect_ratio = max_aspect_ratio if max_aspect_ratio is not None else float(10.) # no dimension
         self.min_aspect_ratio = min_aspect_ratio if min_aspect_ratio is not None else float(6.) # no dimension
-        self.major_radius_plasma = major_radius_plasma if major_radius_plasma is not None else float(0.5) # meter
-        self.minor_radius_plasma = minor_radius_plasma if minor_radius_plasma is not None else float(0.08) # meter
+        self.radius_major = radius_major if radius_major is not None else float(0.5) # meter
+        self.radius_minor = radius_minor if radius_minor is not None else float(0.08) # meter
         
-        # Coil assumptions
+        #dimensions parameters
+        self.len_coil = float(0.) # m
+        self.length_of_circuit = float(0.) # m
+        self.radius_within = float(0.) # mm available space within torus
+        self.total_circumference_of_coils_within = float(0.) # mm
+        self.total_coil_volume_within = float(0.) # mm²
+        self.number_of_coils = int(0) 
+        self.volume_within = float(0.) # mm²
+        self.aspect_ratio = float(0.) # no dimension
+
+        # fields and stuff
+        self.frequency_rotation = frequency_rotation if frequency_rotation is not None else float(2.45e9)#Hz
         self.number_of_coils_per_circuit = number_of_coils_per_circuit if number_of_coils_per_circuit is not None else int(10)
         self.number_of_circuits = number_of_circuits if number_of_circuits is not None else int(4)
         self.number_of_windings_x = number_of_windings_x if number_of_windings_x is not None else int(3)
@@ -33,40 +46,36 @@ class StelleratorDesign:
         self.winding_radius = winding_radius if winding_radius is not None else float(0.015) # m
         self.cooling_radius = cooling_radius if cooling_radius is not None else float(0.02) # cooling coil + winding coil
         
-        # Calculated from basis variables
-        self.max_current_per_winding = float(0.) # A
-        self.length_of_winding = float(0.) # m
-        self.length_of_coil = float(0.) # m
-        self.length_of_circuit = float(0.) # m
-        self.current_within = float(0.) # kA, within labels the stuff which is at positions < major radius
-        self.radius_within = float(0.) # mm available space within torus
-        self.total_circumference_of_coils_within = float(0.) # mm
-        self.total_coil_volume_within = float(0.) # mm²
-        self.number_of_coils = int(0) 
-        
-        # Calculated from precalculations
-        self.current_per_coil = float(0.) # kA
-        self.current_per_winding = float(0.) # kA
-        self.volume_within = float(0.) # mm²
-        self.aspect_ratio = float(0.) # no dimension
+        # fields and stuff to calculate
+        self.max_I_winding = float(0.) # A
+        self.I_linking = float(0.) # kA, within labels the stuff which is at positions < major radius
+        self.I_coil = float(0.) # kA
+        self.I_winding = float(0.) # kA
         self.resistance_per_circuit = float(0.) # ohm
         self.voltage_per_circuit = float(0.) # Volt
+        self.B_toroidal = float(0.) #Tesla #B_toroidal if B_toroidal is not None else float(87.3/1000) # T
+        self.angular_frequency_rotation = float(0.) # s^(-1)
 
+        self.material = material
+        if self.material == 'copper':
+            self.specific_resistance = 1.68e-8#Ohm*m @ 77°C
+		    # heat capacity =
+		    # heat conduction =
+        elif self.material == "aluminum" or "aluminium":
+            self.specific_resistance = 3.875e-8
+		    #self.specific_resistance = 3.875*10**(-8)#Ohm*m @ 77°C (source: https://hypertextbook.com/facts/2004/ValPolyakov.shtml)
+		    # heat capacity = 
+		    # heat conduction = 
+        if self.specific_resistance == None:
+            raise Exception('Missing conductor material properties')
     ###########################################################################################
     
     #getters
 
     ###########################################################################################
 
-    #basic calculations, don't need any precalculations
-    def get_max_current_per_winding(self):
-        '''Ampere'''
-        return self.max_current_per_m_2 / (math.pi * self.winding_radius**2)
-
-    def get_length_of_winding(self):
-        return 2 * math.pi * self.major_winding_radius
-
-    def get_length_of_coil(self):
+    #dimension calculations
+    def get_len_coil(self):
         return self.number_of_windings_x * self.number_of_windings_y * 2 * math.pi * self.major_winding_radius
 
     def get_number_of_coils(self):
@@ -75,24 +84,20 @@ class StelleratorDesign:
 
     def get_circumference_within(self):
         '''gives the radius within, based on outer dimensions in mm'''
-        radius = self.major_radius_plasma - self.minor_radius_plasma - 2 * self.number_of_windings_y * self.winding_radius
+        radius = self.radius_major - self.radius_minor - 2 * self.number_of_windings_y * self.winding_radius
         return 2 * math.pi * radius
-
-    def get_current_within(self):
-        '''gives the current on the inside of the plasma loop in kA, with Formula mu_0 I = int B * dl'''
-        return self.B_middle * self.major_radius_plasma * 2 * math.pi / self.mu_0
 
     def get_aspect_ratio(self):
         '''dimensionless, R_0/a, integrated control, should be 6 ~ 10'''
-        if (self.major_radius_plasma / self.minor_radius_plasma > self.max_aspect_ratio or
-            self.major_radius_plasma / self.minor_radius_plasma < self.min_aspect_ratio):
-            print(f'ERROR: aspect ratio out of boundary; aspect ratio = ', self.major_radius_plasma / self.minor_radius_plasma)
+        if (self.radius_major / self.radius_minor > self.max_aspect_ratio or
+            self.radius_major / self.radius_minor < self.min_aspect_ratio):
+            print(f'ERROR: aspect ratio out of boundary; aspect ratio = ', self.radius_major / self.radius_minor)
             #sys.exit()
-        return self.major_radius_plasma / self.minor_radius_plasma
+        return self.radius_major / self.radius_minor
 
     def get_radius_within(self):
         '''the available space on the inside of the torus'''
-        return 2 * math.pi * (self.major_radius_plasma - self.minor_radius_plasma - self.cooling_radius * 2 * self.number_of_windings_y)
+        return 2 * math.pi * (self.radius_major - self.radius_minor - self.cooling_radius * 2 * self.number_of_windings_y)
 
     def get_total_coil_volume_within(self):
         '''gives'''
@@ -113,15 +118,28 @@ class StelleratorDesign:
             #sys.exit()
         return total_circumference
 
-    def get_current_per_coil(self):
-        '''kA'''
-        return self.get_current_within() / self.get_number_of_coils()
+    #current and fields
+    def get_B_toroidal(self):
+        "Tesla"
+        return self.electron_mass*self.frequency_rotation*2*math.pi/self.electron_charge
 
-    def get_current_per_winding(self):
+    def get_max_I_winding(self):
+        '''Ampere'''
+        return self.max_current_per_m_2 / (math.pi * self.winding_radius**2)
+    
+    def get_I_linking(self):
+        '''gives the current on the inside of the plasma loop in kA, with Formula mu_0 I = int B * dl'''
+        return self.B_toroidal * self.radius_major * 2 * math.pi / self.mu_0
+
+    def get_I_coil(self):
+        '''kA'''
+        return self.get_I_linking() / self.get_number_of_coils()
+
+    def get_I_winding(self):
         '''kA, integrated control, does not exceed max_current'''
-        c_p_w = self.get_current_within() / (self.get_number_of_coils() * self.number_of_windings_x * self.number_of_windings_y)
-        if (c_p_w > self.max_current_per_winding):
-            print(f'ERROR: too much current per winding; maximal current = ', self.max_current_per_winding, 'actual current = ', c_p_w)
+        c_p_w = self.get_I_linking() / (self.get_number_of_coils() * self.number_of_windings_x * self.number_of_windings_y)
+        if (c_p_w > self.max_I_winding):
+            print(f'ERROR: too much current per winding; maximal current = ', self.max_I_winding, 'actual current = ', c_p_w)
             sys.exit()
         return c_p_w
 
@@ -134,15 +152,15 @@ class StelleratorDesign:
     #setters
 
     ##############################################################################################
-    
-    def set_B_middle(self, value):
-        self.B_middle = value
+
+    def set_B_toroidal(self, value):
+        self.B_toroidal = value
 
     def set_mu_0(self, value):
         self.mu_0 = value
 
-    def set_max_radius(self, value):
-        self.max_radius = value
+    def set_diam_max(self, value):
+        self.diam_max = value
 
     def set_max_height(self, value):
         self.max_height = value
@@ -153,11 +171,11 @@ class StelleratorDesign:
     def set_min_aspect_ratio(self, value):
         self.min_aspect_ratio = value
 
-    def set_major_radius_plasma(self, value):
-        self.major_radius_plasma = value
+    def set_radius_major(self, value):
+        self.radius_major = value
 
-    def set_minor_radius_plasma(self, value):
-        self.minor_radius_plasma = value
+    def set_radius_minor(self, value):
+        self.radius_minor = value
 
     def set_number_of_coils_per_circuit(self, value):
         self.number_of_coils_per_circuit = value
@@ -186,20 +204,17 @@ class StelleratorDesign:
     def set_cooling_radius(self, value):
         self.cooling_radius = value
 
-    def set_max_current_per_winding(self, value):
-        self.max_current_per_winding = value
+    def set_max_I_winding(self, value):
+        self.max_I_winding = value
 
-    def set_length_of_winding(self, value):
-        self.length_of_winding = value
-
-    def set_length_of_coil(self, value):
-        self.length_of_coil = value
+    def set_len_coil(self, value):
+        self.len_coil = value
 
     def set_length_of_circuit(self, value):
         self.length_of_circuit = value
 
-    def set_current_within(self, value):
-        self.current_within = value
+    def set_I_linking(self, value):
+        self.I_linking = value
 
     def set_radius_within(self, value):
         self.radius_within = value
@@ -213,11 +228,11 @@ class StelleratorDesign:
     def set_number_of_coils(self, value):
         self.number_of_coils = value
 
-    def set_current_per_coil(self, value):
-        self.current_per_coil = value
+    def set_I_coil(self, value):
+        self.I_coil = value
 
-    def set_current_per_winding(self, value):
-        self.current_per_winding = value
+    def set_I_winding(self, value):
+        self.I_winding = value
 
     def set_volume_within(self, value):
         self.volume_within = value
@@ -230,6 +245,29 @@ class StelleratorDesign:
 
     def set_voltage_per_circuit(self, value):
         self.voltage_per_circuit = value
+
+    def set_frequency_rotation(self, value):
+        self.frequency_rotation = value
+
+    def set_angular_frequency_rotation(self, value):
+        self.angular_frequency_rotation = value # s^(-1)
+        
+    def set_B_toroidal(self, value):
+        self.B_toroidal = value #Tesla
+
+    def set_material(self, value):
+        if value == 'copper':
+            self.specific_resistance = 1.68e-8#Ohm*m @ 77°C
+		    # heat capacity =
+		    # heat conduction =
+        elif value == "aluminum" or "aluminium":
+            self.specific_resistance = 3.875e-8
+		    #self.specific_resistance = 3.875*10**(-8)#Ohm*m @ 77°C (source: https://hypertextbook.com/facts/2004/ValPolyakov.shtml)
+		    # heat capacity = 
+		    # heat conduction = 
+        if self.specific_resistance == None:
+            raise Exception('Missing conductor material properties')
+        self.material = value
 
     ##############################################################################################
 
--- completely new/main.py
+++ completely new/main.py
@@ -4,33 +4,33 @@ import sys
 from definitions import *
 
 def calculations(stellarator):
-    stellarator.max_current_per_winding = stellarator.get_max_current_per_winding()
-    stellarator.length_of_winding = stellarator.get_length_of_winding()
-    stellarator.length_of_coil = stellarator.get_length_of_coil()
+    stellarator.B_toroidal = stellarator.get_B_toroidal()
+    stellarator.max_I_winding = stellarator.get_max_I_winding()
+    stellarator.len_coil = stellarator.get_len_coil()
     stellarator.number_of_coils = stellarator.get_number_of_coils()
     stellarator.circumference_within = stellarator.get_circumference_within()
-    stellarator.current_within = stellarator.get_current_within()
+    stellarator.I_linking = stellarator.get_I_linking()
     stellarator.aspect_ratio = stellarator.get_aspect_ratio()
     stellarator.radius_within = stellarator.get_radius_within()
     stellarator.total_coil_volume_within = stellarator.get_total_coil_volume_within()
     stellarator.length_of_circuit = stellarator.get_length_of_circuit()
     stellarator.volume_of_coils_within = stellarator.get_volume_of_coils_within()
     stellarator.total_coil_radius_within = stellarator.get_total_coil_radius_within()
-    stellarator.current_per_coil = stellarator.get_current_per_coil()
-    stellarator.current_per_winding = stellarator.get_current_per_winding()
+    stellarator.I_coil = stellarator.get_I_coil()
+    stellarator.I_winding = stellarator.get_I_winding()
     stellarator.resistance_per_circuit = stellarator.get_resistance_per_circuit()
 
-
+def controll():
+    return True
 
 def main():
-    stellerator = StelleratorDesign(B_middle=None, mu_0=None, max_radius=None, max_height=None,
-                 max_aspect_ratio=None, min_aspect_ratio=None, major_radius_plasma=None,
-                 minor_radius_plasma=None, number_of_coils_per_circuit=None,
+    stellarator = StellaratorDesign(material="aluminium", diam_max=None, max_height=None,
+                 max_aspect_ratio=None, min_aspect_ratio=None, radius_major=None,
+                 radius_minor=None, number_of_coils_per_circuit=None,
                  number_of_circuits=None, number_of_windings_x=None, number_of_windings_y=None,
                  max_current_per_m_2=None, specific_resistance=None, major_winding_radius=None,
                  winding_radius=None, cooling_radius=None)
-    stellerator.print_parameters()
-    calculations(stellerator)
-    stellerator.print_parameters()
+    calculations(stellarator)
+    stellarator.print_parameters()
 
 main()
