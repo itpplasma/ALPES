e406ff42e3616451fa29d346485fc4d66cbea201
Jan Priedigkeit <priedigkeit@student.tugraz.at>
2024-06-02 17:14:05 +0200

    crosssection calculator

---
+++ coils_xyz/coil_distance.py
@@ -0,0 +1,15 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+import glob
+
+files = glob.glob("*.txt")
+print(files)
+
+# for file in os.listdir('.'):
+#     if file[:16] == "coil_coordinates":
+#         coil_coordinates = np.loadtxt(file)
+#         print(file)
+#         print(coil_coordinates)
+
+
